2ea254b3f57467f77e2005f5bf9540f962fcc9fe
karanokk <karanokk@icloud.com>
2020-08-15 12:57:32 +0800

    feat(tools): add force update option

--- tools/gen_servants.py
+++ tools/gen_servants.py
@@ -17,18 +17,23 @@ def main():
     parser = OptionParser()
     parser.add_option("-o", "--output_path", dest="output_path")
     parser.add_option("-l", "--log_file", dest="log_file")
+    parser.add_option("-f", '--forceUpdate', action='store_true', dest='force_update', default=False)
 
     options, _ = parser.parse_args()
     output_path = options.output_path
     log_file = options.log_file
+    force_update = options.force_update
 
     logger = logging.getLogger(__name__)
 
     dirty_servants = mooncell_text.dirty_servants()
 
     if not dirty_servants:
-        logger.info('Servants no update.')
-        return
+        if force_update:
+            logger.info('Force updating.')
+        else:
+            logger.info('Servants no update.')
+            return
         
     servants_dir = os.path.join(output_path, 'servants')
     _create_dir_if_not_exists(servants_dir)
