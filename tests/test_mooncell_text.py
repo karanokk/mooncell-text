8958965af04617c36484e49080ba963aa088bd0a
karanokk <karanokk@icloud.com>
2020-08-15 12:50:56 +0800

    test: remove test_mooncell_text

--- tests/test_mooncell_text.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from mooncell_text.MooncellText import MooncellText
-
-class MooncellTextTest(unittest.TestCase):
-    def test_all_servants(self):
-        mt = MooncellText()
-        # mt.extract_servant('玛修·基列莱特')
-        mt.extract_all_servants_to_file('/Users/byunfi/Desktop/extract')
